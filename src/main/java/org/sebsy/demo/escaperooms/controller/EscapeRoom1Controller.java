065cfb78dae4d33bb55caa823ddcc66fce860aa4
kbellouard <killian.bellouard@getnobullshit.com>
2025-03-18 14:37:30 +0100

    ADV Ex 05

--- src/main/java/org/sebsy/demo/escaperooms/controller/EscapeRoom1Controller.java
+++ src/main/java/org/sebsy/demo/escaperooms/controller/EscapeRoom1Controller.java
@@ -1,5 +1,11 @@
 package org.sebsy.demo.escaperooms.controller;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+@Profile(value = "passage")
 public class EscapeRoom1Controller {
 
     public void entreeSalle1() {
